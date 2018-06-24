d6fc6c63ded6718d6e0827d1113cb5739aa4d401
NITHIN JOHN <Anu@anna91>
2018-06-24 03:34:24 +0100

    Contacts access permission asked on launch

--- app/src/main/java/com/example/nithinjohn/tabbedscreen/Loader.kt
+++ app/src/main/java/com/example/nithinjohn/tabbedscreen/Loader.kt
@@ -1,8 +1,11 @@
 package com.example.nithinjohn.tabbedscreen
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Handler
+import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
 import android.view.Window
 import android.view.WindowManager
@@ -12,12 +15,18 @@ import kotlinx.android.synthetic.main.activity_loader.*
 class Loader : AppCompatActivity() {
 
     private val SPLASH_TIME_OUT = 5000L
+    val REQUEST_PERMISSION = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         this.requestWindowFeature(Window.FEATURE_NO_TITLE)
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_CONTACTS), REQUEST_PERMISSION)
+        }
+
         setContentView(R.layout.activity_loader)
         Glide.with(this)
                 .asGif()
