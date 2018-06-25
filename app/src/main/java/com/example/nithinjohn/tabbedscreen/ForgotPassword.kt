490ca4e643467a7eb31e6881aff64d1fc27cc5c1
NithinMJ <30488140+NithinMJ@users.noreply.github.com>
2018-06-25 16:10:39 +0100

    Updated Password Reset Methodology

---
+++ app/src/main/java/com/example/nithinjohn/tabbedscreen/ForgotPassword.kt
@@ -0,0 +1,40 @@
+package com.example.nithinjohn.tabbedscreen
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_forgot_password.*
+
+const val TAG: String = "TAG"
+
+class ForgotPassword : AppCompatActivity() {
+
+    private var mAuth: FirebaseAuth? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forgot_password)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        reset_pwd.setOnClickListener {
+            mAuth?.sendPasswordResetEmail(pwd_reset_email.text.toString())?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "Email Sent")
+                    Toast.makeText(applicationContext, "Email Link sent to reset password", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this@ForgotPassword, LoginActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+        }
+
+    }
+
+    override fun onBackPressed() {
+        val intent = Intent(this@ForgotPassword, LoginActivity::class.java)
+        startActivity(intent)
+    }
+}
